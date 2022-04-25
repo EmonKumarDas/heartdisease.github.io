c35fe519127c7f2dae4833f64011588461a7ca0a
emon kumar das <89456741+EmonKumarDas@users.noreply.github.com>
2022-04-25 10:22:48 +0600

    Update heart.py

--- heart.py
+++ heart.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import sklearn
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
